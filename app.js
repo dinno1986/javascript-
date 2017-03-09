506478c6d24a21f97da3ca18b337c41573d540ce
dinno1986 <dimitri.castell@hotmail.fr>
2017-03-09 11:59:14 +0100

    rajout soustraction

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+// faire une addition
+
 function addition (a,b ) {
-	console.log(a+b); 
+	return(a+b); 
 };
 
-addition(2,4); 
+console.log (addition(2,4)); 
+
+// faire une soustraction
+
+function substraction(a,b) {
+	return(a-b);
+};
+
+console.log (substraction(6,1));
