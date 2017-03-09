8c2013a5968f7d6caa8f482d3145b71d5b287799
dinno1986 <dimitri.castell@hotmail.fr>
2017-03-09 14:01:29 +0100

    modif

---
+++ app.js
@@ -0,0 +1,34 @@
+// faire une addition
+
+function addition (a,b ) {
+	return(a+b); 
+};
+
+console.log (addition(2,4)); 
+
+// faire une soustraction
+
+function substraction(a,b) {
+	return(a-b);
+};
+
+console.log (substraction(6,1));
+
+// faire une multiplication
+
+function multiplication(a,b) {
+	return(a*b);
+};
+
+console.log (multiplication(9,4));
+
+// faire une division
+
+function division(a,b) {
+	if (b > 0)
+		console.log(a/b);
+	else
+		console.log("Can't divide by 0");}
+
+	division(21,4);
+
